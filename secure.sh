52142df9ac37f39b7992280147f13e00d8fc43a9
Bruno dos Santos <brunosantos_90@hotmail.com>
2023-05-24 19:27:26 -0300

    Create function checa_internet

--- secure.sh
+++ secure.sh
@@ -6,9 +6,12 @@ BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 # Internet conectando?
+checa_internet () {
 if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
   echo -e "${RED}   [ERROR] - Seu computador não tem conexão com a Internet. Verifique os cabos e o modem.${NC}";sleep 2
   exit 1
 else
   echo -e "${GREEN}   [CHECK] - Conexão com a Internet funcionando normalmente.${NC}";sleep 2
 fi
+}
+
