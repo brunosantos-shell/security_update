4edbd5ba6be6da24309d6867ce1b3ee271196973
Bruno dos Santos <brunosantos_90@hotmail.com>
2023-05-24 19:23:38 -0300

    Check internet connection

---
+++ secure.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+# Internet conectando?
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${RED}   [ERROR] - Seu computador não tem conexão com a Internet. Verifique os cabos e o modem.${NC}";sleep 2
+  exit 1
+else
+  echo -e "${GREEN}   [CHECK] - Conexão com a Internet funcionando normalmente.${NC}";sleep 2
+fi
